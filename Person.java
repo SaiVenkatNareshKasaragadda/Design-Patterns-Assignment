fdc5c9512336abb64a17aeed10a7c733625aac28
SaiVenkatNareshKasaragadda <112650297+SaiVenkatNareshKasaragadda@users.noreply.github.com>
2022-10-20 21:59:24 -0700

    Add files via upload
    Implemented Iterator and Visitor Pattern

---
+++ Person.java
@@ -0,0 +1,21 @@
+
+public abstract class Person {
+    private String username;
+    private String password;
+    private Facade facade;
+
+
+public Person(String username, String password, Facade facade){
+    this.username = username;
+    this.password = password;
+    this.facade = facade;
+}
+private ProductMenu theProductMenu;
+public abstract void showMenu();
+public void showAddButton(){}
+public void showViewButton(){}
+public void showRadioButton(){}
+public void showLabels(){}
+public abstract ProductMenu creatProductMenu(); 
+
+}
